e53d66b01f6ae50f64889568f40bc3c5979480df
Monty V <135301818+ghostlight-goat@users.noreply.github.com>
2026-04-01 19:51:40 -0700

    jump update
    adjusted jump velocity to allow for higher jumps

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var screen_size
 
